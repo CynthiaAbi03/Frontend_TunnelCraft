e67bca2cc34f65f86bd864c1dc46c6f9b1f6a5be
CynthiaAbi03 <119008657+CynthiaAbi03@users.noreply.github.com>
2024-02-19 08:16:21 +0100

    project modifications

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,5 @@ export { default as LandingPage} from './Landing_page/landing_page';
 export {default as AcceuilPage} from './Acceuil_page/acceuil'
 export {default as ApplicationsPage} from './Applications_page/applications';
 export {default as CommencerPage} from './Commencer_page/commencer';
-export {default as FacturationPage} from './Facturation_page/Facturation';
+export {default as FacturationPage} from './Facturation_page/facturation';
 export {default as TokenPage} from './Token_page/token'
